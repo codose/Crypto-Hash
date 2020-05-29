862577cc85e9902c0ee11c334784a4e9b44bb200
Osemwingie Oshodin <osemwingieoshodin@gmail.com>
2020-05-29 13:35:30 +0100

    Successfully made the API call

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -19,9 +19,9 @@ final List<MaterialColor> _colors = [
 
 class _HomePageState extends State<HomePage> {
   @override
-  void initState() async {
+  void initState() {
     super.initState();
-    currencies = await getCurrencies();
+    this.getCurrencies();
   }
 
   @override
@@ -32,32 +32,73 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Future<List> getCurrencies() async {
-    String url = "https://api.coinmarketcap.com/v1/ticker/?limit=50";
-    http.Response response = await http.get(url);
-    return jsonDecode(response.body);
+  Future<String> getCurrencies() async {
+    String url =
+        "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+    http.Response response = await http.get(
+      url,
+      headers: {
+        "X-CMC_PRO_API_KEY": "98812d22-f4be-46ba-b9f2-683e59fc2c2e",
+        "Accept": "application/json"
+      },
+    );
+
+    setState(() {
+      var convertToJson = jsonDecode(response.body);
+      currencies = convertToJson['data'];
+      print(currencies);
+    });
+
+    return "Success";
   }
 }
 
 Widget _cryptoWidget() {
   return Container(
-    child: Flexible(
-        child: ListView.builder(
-            itemCount: currencies.length,
-            itemBuilder: (BuildContext context, int index) {
-              final Map currency = currencies[index];
-              final MaterialColor color = _colors[index % _colors.length];
-              return _getListItemUi(currency, color);
-            })),
-  );
+      child: Column(
+    children: <Widget>[
+      Flexible(
+          child: ListView.builder(
+              itemCount: currencies == null ? 0 : currencies.length,
+              itemBuilder: (BuildContext context, int index) {
+                final Map currency = currencies[index];
+                final MaterialColor color = _colors[index % _colors.length];
+                return _getListItemUi(currency, color);
+              })),
+    ],
+  ));
 }
 
 ListTile _getListItemUi(Map currency, MaterialColor color) {
+  var usd = currency['quote']['USD'];
   return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: color,
-        child: Text(currency['name'][0]),
-      ),
-      title: Text(currency['name']),
+    leading: CircleAvatar(
+      backgroundColor: color,
+      child: Text(currency['name'][0]),
+    ),
+    title: Text(
+      currency['name'],
+      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    ),
+    subtitle: _getSubTitleText(usd['price'], usd['percent_change_1h']),
+    isThreeLine: true,
   );
+}
+
+Widget _getSubTitleText(double priceUSD, double percentChange) {
+  TextSpan priceTextWidget =
+      TextSpan(text: "\$$priceUSD", style: TextStyle(color: Colors.black));
+  String percentageChangeText = "1 hour : $percentChange%";
+  TextSpan percentChangeWidget;
+
+  if (percentChange > 0) {
+    percentChangeWidget = TextSpan(
+        text: percentageChangeText, style: TextStyle(color: Colors.green));
+  } else {
+    percentChangeWidget = TextSpan(
+        text: percentageChangeText, style: TextStyle(color: Colors.red));
+  }
+
+  return RichText(
+      text: TextSpan(children: [priceTextWidget, percentChangeWidget]));
 }
